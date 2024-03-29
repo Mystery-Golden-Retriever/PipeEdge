c5aa283bdc78cacc2afa0efe2bc7ea79082d8af2
liuzeli <liuzeli0708@gmail.com>
2024-03-29 14:56:42 -0700

    upload mixed-quant

--- runtime.py
+++ runtime.py
@@ -12,13 +12,14 @@ from PIL import Image
 import requests
 import torch
 from torch.utils.data import DataLoader, Dataset
+from torchvision.datasets import ImageNet
 from transformers import AutoTokenizer, DeiTFeatureExtractor, ViTFeatureExtractor
 from pipeedge.comm.p2p import DistP2pContext
 from pipeedge.comm.rpc import DistRpcContext, tensorpipe_rpc_backend_options_factory
 from pipeedge import models
-from pipeedge.quantization.basic_op import (
-    compression_factor, tensor_encode_outerdim, tensor_decode_outerdim
-)
+import pipeedge.quantization.basic_op_float as basic_op_float
+import pipeedge.quantization.basic_op_integer as basic_op_integer
+from pipeedge.quantization.basic_op_integer import compression_factor
 from pipeedge.quantization.clamp_op import clamp_banner2019_gelu, clamp_banner2019_laplace
 from pipeedge.sched.scheduler import sched_pipeline
 import devices
@@ -27,11 +28,23 @@ import monitoring
 from utils import data, threads
 from utils import quant as quantutil
 
+from torchvision import transforms
+
 logger = logging.getLogger(__name__)
 
 ## ground truth: Egyptian cat
-IMG_URL = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-IMG_LABEL_IDX = 285
+# IMG_URL = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+# IMG_LABEL_IDX = 285
+
+# More Ground Truth Samples From https://github.com/EliSchwartz/imagenet-sample-images/tree/master
+# IMG_URL = 'https://raw.githubusercontent.com/EliSchwartz/imagenet-sample-images/master/n01440764_tench.JPEG'
+# IMG_LABEL_IDX = 0
+
+# IMG_URL = 'https://raw.githubusercontent.com/EliSchwartz/imagenet-sample-images/master/n01514859_hen.JPEG'
+# IMG_LABEL_IDX = 8
+
+IMG_URL = 'https://raw.githubusercontent.com/EliSchwartz/imagenet-sample-images/master/n01580077_jay.JPEG'
+IMG_LABEL_IDX = 17
 
 CMD_STOP = 0
 CMD_SCHED = 1
@@ -77,20 +90,27 @@ def forward_hook_quant_encode(module, _input_arg, output: Union[torch.Tensor, Tu
         output = (output,)
     assert isinstance(output, tuple)
     quant_bit = module.quant_bit.item()
+    e_bit=module.e_bit.item()
+    is_Clamp=module.is_Clamp.item()
+    assert 0<=e_bit<=quant_bit-1
     comm_tuple = []
     for tensor in output:
         assert isinstance(tensor, torch.Tensor)
-        if quant_bit > 0:
-            clamp = clamp_banner2019_laplace if tensor.min() < 0.2 else clamp_banner2019_gelu
-            tensor = clamp(tensor, quant_bit)
-        stacked_tensor = tensor_encode_outerdim(tensor, quant_bit)
+        if is_Clamp:
+            if quant_bit > 0:
+                clamp = clamp_banner2019_laplace if tensor.min() < 0.2 else clamp_banner2019_gelu
+                tensor = clamp(tensor, quant_bit)
+        if e_bit==0:
+            stacked_tensor = basic_op_integer.tensor_encode_outerdim(tensor, quant_bit)
+        else:
+            stacked_tensor = basic_op_float.tensor_encode_outerdim(tensor, quant_bit,e_bit)
         comm_tuple += stacked_tensor
     # Measure work as the microbatch size, but quantization only does work if quant_bit > 0.
     n_items = models.get_microbatch_size(output[0], verify=True) if quant_bit > 0 else 0
     monitoring.iteration(MONITORING_KEY_QUANT_ENCODE, work=n_items, accuracy=quant_bit)
     return tuple(comm_tuple)
 
-def forward_pre_hook_quant_decode(_module, input_arg: Tuple[Tuple[torch.Tensor, ...]]):
+def forward_pre_hook_quant_decode(module, input_arg: Tuple[Tuple[torch.Tensor, ...]]):
     """decode tensor in the preforward hook (before each module)"""
     monitoring.iteration_start(MONITORING_KEY_QUANT_DECODE)
     assert isinstance(input_arg, tuple)
@@ -100,11 +120,16 @@ def forward_pre_hook_quant_decode(_module, input_arg: Tuple[Tuple[torch.Tensor, 
     assert isinstance(input_tensors, tuple)
     assert len(input_tensors)%5 == 0
     assert len(input_tensors) >= 5
-    quant_bit = input_tensors[4][0].item() # assume the same quantization bitwidth for all items
+    quant_bit = module.quant_bit.item()
+    e_bit=module.e_bit.item()
+    # quant_bit = input_tensors[4][0].item() # assume the same quantization bitwidth for all items
     forward_tensor = []
     for i in range(len(input_tensors) // 5):
         input_tensor = input_tensors[i*5:i*5+5]
-        batched_tensor = tensor_decode_outerdim(input_tensor)
+        if e_bit==0:
+            batched_tensor = basic_op_integer.tensor_decode_outerdim(input_tensor)
+        else:
+            batched_tensor = basic_op_float.tensor_decode_outerdim(input_tensor)
         forward_tensor.append(batched_tensor)
     # Return value(s) should be wrapped in an outer tuple, like input_arg
     # The tuple will be unpacked when forward() is invoked, which must yield a single parameter
@@ -362,6 +387,15 @@ def load_dataset(dataset_cfg: dict, model_name: str, batch_size: int, ubatch_siz
                           'facebook/deit-small-distilled-patch16-224',
                           'facebook/deit-tiny-distilled-patch16-224']:
             feature_extractor = DeiTFeatureExtractor.from_pretrained(model_name)
+
+        elif model_name.startswith('torchvision'):
+            feature_extractor = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
+            transforms.Lambda(lambda x: x.unsqueeze(0))
+            ])
         else:
             feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
         return feature_extractor
@@ -370,34 +404,53 @@ def load_dataset(dataset_cfg: dict, model_name: str, batch_size: int, ubatch_siz
     dataset_split = dataset_cfg['split']
     indices = dataset_cfg['indices']
     dataset_shuffle = dataset_cfg['shuffle']
-    if dataset_name == 'CoLA':
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        dataset = data.load_dataset_glue(tokenizer, 'cola', dataset_split, ubatch_size)
-        dataset = data.load_dataset_subset(dataset, indices=indices, max_size=batch_size,
-                                           shuffle=dataset_shuffle)
-    elif dataset_name == 'ImageNet':
-        if dataset_root is None:
-            dataset_root = 'ImageNet'
-            logging.info("Dataset root not set, assuming: %s", dataset_root)
-        feature_extractor = _get_feature_extractor()
-        dataset = data.load_dataset_imagenet(feature_extractor, dataset_root, split=dataset_split)
-        dataset = data.load_dataset_subset(dataset, indices=indices, max_size=batch_size,
-                                           shuffle=dataset_shuffle)
-    elif model_name in ['bert-base-uncased', 'bert-large-uncased',
-                        'textattack/bert-base-uncased-CoLA']:
-        with np.load("bert_input.npz") as bert_inputs:
-            inputs_sentence = list(bert_inputs['input'][:batch_size])
-            labels = torch.tensor(bert_inputs['label'][:batch_size])
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        inputs = tokenizer(inputs_sentence, padding=True, truncation=True, return_tensors="pt")['input_ids']
-        dataset = data.RolloverTensorDataset(batch_size, inputs, labels)
+    if model_name.startswith('torchvision'):
+        # for cnn models
+        if dataset_name == 'ImageNet':
+            transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
+            # transforms.Lambda(lambda x: x.unsqueeze(0))
+            ])
+            dataset = ImageNet(dataset_root, split=dataset_split, transform=transform)
+        else:
+            image = Image.open(requests.get(IMG_URL, stream=True, timeout=60).raw)
+            feature_extractor = _get_feature_extractor()
+            inputs = feature_extractor(image)
+            dataset = data.RolloverTensorDataset(batch_size, inputs, torch.tensor([IMG_LABEL_IDX]))
     else:
-        feature_extractor = _get_feature_extractor()
-        ## random data
-        # image = torch.randn(3, 384, 384)
-        image = Image.open(requests.get(IMG_URL, stream=True, timeout=60).raw)
-        inputs = feature_extractor(images=[image], return_tensors="pt")['pixel_values']
-        dataset = data.RolloverTensorDataset(batch_size, inputs, torch.tensor([IMG_LABEL_IDX]))
+        # for transformer models
+        if dataset_name == 'CoLA':
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            dataset = data.load_dataset_glue(tokenizer, 'cola', dataset_split, ubatch_size)
+            dataset = data.load_dataset_subset(dataset, indices=indices, max_size=batch_size,
+                                            shuffle=dataset_shuffle)
+        elif dataset_name == 'ImageNet':
+            if dataset_root is None:
+                dataset_root = 'ImageNet'
+                logging.info("Dataset root not set, assuming: %s", dataset_root)
+            feature_extractor = _get_feature_extractor()
+            dataset = data.load_dataset_imagenet(feature_extractor, dataset_root, split=dataset_split)
+            dataset = data.load_dataset_subset(dataset, indices=indices, max_size=batch_size,
+                                            shuffle=dataset_shuffle)
+        elif model_name in ['bert-base-uncased', 'bert-large-uncased',
+                            'textattack/bert-base-uncased-CoLA']:
+            with np.load("bert_input.npz") as bert_inputs:
+                inputs_sentence = list(bert_inputs['input'][:batch_size])
+                labels = torch.tensor(bert_inputs['label'][:batch_size])
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            inputs = tokenizer(inputs_sentence, padding=True, truncation=True, return_tensors="pt")['input_ids']
+            dataset = data.RolloverTensorDataset(batch_size, inputs, labels)
+        else:
+            feature_extractor = _get_feature_extractor()
+            ## random data
+            # image = torch.randn(3, 384, 384)
+            image = Image.open(requests.get(IMG_URL, stream=True, timeout=60).raw)
+            inputs = feature_extractor(images=[image], return_tensors="pt")['pixel_values']
+            dataset = data.RolloverTensorDataset(batch_size, inputs, torch.tensor([IMG_LABEL_IDX]))
+    
     return dataset
 
 
@@ -716,6 +769,7 @@ def main() -> None:
     logger.info("Device: %s", devices.DEVICE)
     logger.debug("# parallel intra nodes threads: %d", torch.get_num_threads())
     logger.debug("# parallel inter nodes threads: %d", torch.get_num_interop_threads())
+    
     if args.comm == 'p2p':
         run_pipeline_p2p(args.worldsize, args.rank, args.model_name, args.model_file,
                          args.batch_size, args.ubatch_size, partition, quant, rank_order,
