6153ff27efb5508c604aeca474c7b1a237f872f7
Haonan Wang <haonanwa@usc.edu>
2024-06-18 15:03:38 -0700

    runtime.py: merge updates of CNN, but remove all LLM parts, leave it to another runtime_llm file

--- runtime.py
+++ runtime.py
@@ -264,11 +264,7 @@ def handle_results(tensors: torch.Tensor) -> None:
         # Measure accuracy based on label (microbatch ordering must be enforced for correctness).
         ubatch_labels = label_queue.get()
         assert len(tensors) == len(ubatch_labels)
-        #TODO: We should create if-else branch here to know if it is for classification task and generation tasks
-        if len(tensors.shape) == 3:
-            pred = tensors.argmax(dim=1)
-            pred = pred.argmax(dim=1)
-        else: pred = tensors.argmax(dim=1)
+        pred = tensors.argmax(dim=1)
         acc = pred.eq(ubatch_labels).sum().item()
     monitoring.iteration(MONITORING_KEY_OUTPUT, work=n_items, accuracy=acc, safe=False)
     logger.info("outputs is %s", tensors)
@@ -397,60 +393,34 @@ def load_dataset(dataset_cfg: dict, model_name: str, batch_size: int, ubatch_siz
     dataset_split = dataset_cfg['split']
     indices = dataset_cfg['indices']
     dataset_shuffle = dataset_cfg['shuffle']
-    #TODO We need to create workflow taking input from keyboard and tokenize the input to LLMs.
-    if dataset_name == 'CoLA' and model_name == 'facebook/opt-350m':
-        # for llm models
+    if dataset_name == 'CoLA':
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         dataset = data.load_dataset_glue(tokenizer, 'cola', dataset_split, ubatch_size)
         dataset = data.load_dataset_subset(dataset, indices=indices, max_size=batch_size,
                                            shuffle=dataset_shuffle)
-    elif model_name.startswith('torchvision'):
-        # for cnn models
-        if dataset_name == 'ImageNet':
-            transform = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-            # transforms.Lambda(lambda x: x.unsqueeze(0))
-            ])
-            dataset = ImageNet(dataset_root, split=dataset_split, transform=transform)
-        else:
-            image = Image.open(requests.get(IMG_URL, stream=True, timeout=60).raw)
-            feature_extractor = _get_feature_extractor()
-            inputs = feature_extractor(image)
-            dataset = data.RolloverTensorDataset(batch_size, inputs, torch.tensor([IMG_LABEL_IDX]))
+    elif dataset_name == 'ImageNet':
+        if dataset_root is None:
+            dataset_root = 'ImageNet'
+            logging.info("Dataset root not set, assuming: %s", dataset_root)
+        feature_extractor = _get_feature_extractor()
+        dataset = data.load_dataset_imagenet(feature_extractor, dataset_root, split=dataset_split)
+        dataset = data.load_dataset_subset(dataset, indices=indices, max_size=batch_size,
+                                           shuffle=dataset_shuffle)
+    elif model_name in ['bert-base-uncased', 'bert-large-uncased',
+                        'textattack/bert-base-uncased-CoLA']:
+        with np.load("bert_input.npz") as bert_inputs:
+            inputs_sentence = list(bert_inputs['input'][:batch_size])
+            labels = torch.tensor(bert_inputs['label'][:batch_size])
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        inputs = tokenizer(inputs_sentence, padding=True, truncation=True, return_tensors="pt")['input_ids']
+        dataset = data.RolloverTensorDataset(batch_size, inputs, labels)
     else:
-        # for transformer models
-        if dataset_name == 'CoLA':
-            tokenizer = AutoTokenizer.from_pretrained(model_name)
-            dataset = data.load_dataset_glue(tokenizer, 'cola', dataset_split, ubatch_size)
-            dataset = data.load_dataset_subset(dataset, indices=indices, max_size=batch_size,
-                                            shuffle=dataset_shuffle)
-        elif dataset_name == 'ImageNet':
-            if dataset_root is None:
-                dataset_root = 'ImageNet'
-                logging.info("Dataset root not set, assuming: %s", dataset_root)
-            feature_extractor = _get_feature_extractor()
-            dataset = data.load_dataset_imagenet(feature_extractor, dataset_root, split=dataset_split)
-            dataset = data.load_dataset_subset(dataset, indices=indices, max_size=batch_size,
-                                            shuffle=dataset_shuffle)
-        elif model_name in ['bert-base-uncased', 'bert-large-uncased',
-                            'textattack/bert-base-uncased-CoLA']:
-            with np.load("bert_input.npz") as bert_inputs:
-                inputs_sentence = list(bert_inputs['input'][:batch_size])
-                labels = torch.tensor(bert_inputs['label'][:batch_size])
-            tokenizer = AutoTokenizer.from_pretrained(model_name)
-            inputs = tokenizer(inputs_sentence, padding=True, truncation=True, return_tensors="pt")['input_ids']
-            dataset = data.RolloverTensorDataset(batch_size, inputs, labels)
-        else:
-            feature_extractor = _get_feature_extractor()
-            ## random data
-            # image = torch.randn(3, 384, 384)
-            image = Image.open(requests.get(IMG_URL, stream=True, timeout=60).raw)
-            inputs = feature_extractor(images=[image], return_tensors="pt")['pixel_values']
-            dataset = data.RolloverTensorDataset(batch_size, inputs, torch.tensor([IMG_LABEL_IDX]))
-    
+        feature_extractor = _get_feature_extractor()
+        ## random data
+        # image = torch.randn(3, 384, 384)
+        image = Image.open(requests.get(IMG_URL, stream=True, timeout=60).raw)
+        inputs = feature_extractor(images=[image], return_tensors="pt")['pixel_values']
+        dataset = data.RolloverTensorDataset(batch_size, inputs, torch.tensor([IMG_LABEL_IDX]))
     return dataset
 
 
